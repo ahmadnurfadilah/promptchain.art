9642ff2a2464340c65ff4a2d95d607d8a3c756eb
ahmadnurfadilah <ahmadnurfadilah22@gmail.com>
2024-02-12 14:02:14 +0700

    prompt: ui list prompt

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <div className="fixed top-8 inset-x-0 z-10">
       <div className="w-full container px-4">
-        <div className="w-full h-16 bg-white/10 rounded-full backdrop-blur-md border border-white/10 flex items-center justify-between px-4">
+        <div className="w-full h-16 bg-white/10 rounded-full backdrop-blur-xl border border-white/10 flex items-center justify-between px-4">
           <Link href="/" className="flex items-center gap-2 text-primary font-bold">
             <Logo />
             <h2>PromptChain</h2>
@@ -17,7 +17,7 @@ export default function Navbar() {
               <Link href="/" className="hover:text-white hover:underline transition-all hover:underline-offset-8 hover:decoration-2">
                 Home
               </Link>
-              <Link href="/" className="hover:text-white hover:underline transition-all hover:underline-offset-8 hover:decoration-2">
+              <Link href="/prompt" className="hover:text-white hover:underline transition-all hover:underline-offset-8 hover:decoration-2">
                 Discover
               </Link>
               <Link href="/" className="hover:text-white hover:underline transition-all hover:underline-offset-8 hover:decoration-2">
